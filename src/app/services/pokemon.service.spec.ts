47a6676ced88c6f19c4507e463f953bb0ab6da05
Matteo Molinari <matteomolinari93@gmail.com>
2022-12-07 19:43:44 +0100

    Add first service test

---
+++ src/app/services/pokemon.service.spec.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import { PokemonService } from './pokemon.service';
+
+describe('PokemonService', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let service: PokemonService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new PokemonService(httpClientSpy);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should call pokemon api', () => {
+    httpClientSpy.get.withArgs('pokemon?limit=30').and.returnValue(of({}));
+    service.getNext().subscribe(value => {
+      expect(value).not.toBeNull();
+    })
+  })
+});
